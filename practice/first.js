5c6a66e9119bff3301200d7ddcc24c48a2211e1a
kamaalAlag24 <kamaal.alag@thewitslab.com>
2024-05-21 19:11:09 +0530

    Add files via upload

---
+++ practice/first.js
@@ -0,0 +1,64 @@
+console.log("Kamaal Alag");
+console.log("I love js");
+
+// variables
+
+let a = 2;
+let name = "Kamaal";
+name = "Sahil";
+console.log(name);
+const price = 1000;
+//price = 2000; // the price will be printed 1000, because price is a constant it can't be changed.
+console.log(price);
+
+isTrue = false;
+console.log(isTrue);
+
+//blocks 
+
+{
+    let a = 5
+    console.log(a)
+}
+{
+    let a = 10
+    console.log(a)
+}
+
+// Key Value 
+
+const student = {
+    fullName : "Kamaal Alag", //string
+    age : 21,  //number
+    gpa : 4.0,   // number
+    isPass: true // bool
+};
+
+// If i want to use console to print something 
+
+student["age"]+=1; /// if  i want to increment something
+console.log(student["age"]);
+
+// operators 
+
+//Arithmetic operators
+let b = 20;
+let c = 4;
+
+console.log("b = ",b,"c = ",c); 
+console.log("b + c = ",b+c); //24
+console.log("b - c =",b-c);  //16
+console.log("b / c= ",b/c);  //5
+console.log("b % c = ",b%c); //0 
+console.log("b * c = ",b*c); //80
+console.log("b ** c = ",b**c); //160000
+
+
+
+
+
+
+
+
+
+
